03c6f1ce12d309eeded56423e39db72f5d492f35
avinash9-7 <avi89893@gmail.com>
2024-08-15 21:45:13 +0530

    day2

---
+++ day2.js
@@ -0,0 +1,36 @@
+/*Activity2*/
+// task1
+let m=parseInt(prompt("enter a first no."));
+let n=parseInt(prompt("enter a second no."))
+let sum=m+n;
+console.log(sum);
+// task2
+let num1=parseInt(prompt("enter a first no."));
+let num2=parseInt(prompt("enter a second no."))
+let diff=num1-num2;
+console.log(diff);
+// task3
+//task4
+// task5
+// task6
+let a=6;
+a+=2;
+console.log(a);
+a-=2;
+console.log(a);
+// task8
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(m<=n);
+console.log(m<=n);
+// task11
+if(num1>num2 && m<n){
+    console.log("good");
+}
+if(num1<num2 || m>n){
+    console.log("bad");
+}
+if(num1!=num2){
+    console.log("better");
+}
+
