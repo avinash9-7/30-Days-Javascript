76383953bb524f6b961398128aa26c50059ec0c8
avinash9-7 <avi89893@gmail.com>
2024-08-15 20:46:29 +0530

    Update day1.js

---
+++ day1.js
@@ -0,0 +1,20 @@
+/* Activity1 */
+// task1
+var a=2;
+console.log(a);
+// task2
+let b="avinash";
+console.log(b);
+
+/* Activity2 */
+// task3
+const c=true;
+console.log(c);
+
+/* Activity3 */
+// task4
+console.log(typeof(c));
+console.log(typeof(a));
+console.log(typeof(b));
+b="fruits";
+
